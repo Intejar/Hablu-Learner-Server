8111daf08eaa7cdce7088a16a7cac44759ff52ba
Intejar <meherabintejar@gmail.com>
2022-10-27 18:11:41 +0600

    points added in api

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const couseCategory = require('./data/category.json')
-app.get('/', (re1, res)=>{
+const courses = require('./data/courses.json')
+app.get('/', (re1, res) => {
     res.send('course api running');
 });
 
-app.get('/course-category',(req,res)=>{
+app.get('/course-category', (req, res) => {
     res.send(couseCategory)
 })
-app.listen(port,()=>{
-    console.log('course server',port);
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses)
+    }
+    else {
+        const catagory_course = courses.filter(n => n.category_id === id);
+        res.send(catagory_course);
+    }
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c._id === id);
+    res.send(selectedCourse);
+})
+app.listen(port, () => {
+    console.log('course server', port);
 })
